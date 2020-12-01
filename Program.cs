6f25c21451efdb27e5284fca60d3eff5cc522797
SKrauth <spkrauth@gmail.com>
2020-11-30 20:20:21 -0800

    implement coffee making

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace winston
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0){
+                Console.WriteLine("Hello, please enter an argument to begin.");
+                return;
+            }
+            
+            // TODO: better input val and multi arg support
+            if(args[0] == "coffee"){
+                Console.WriteLine("Making Coffee");
+                Coffee.Make();
+            }
+        }
+    }
+
+    static class Coffee
+    {
+        public static void Make(){
+            Console.WriteLine("Heating Water");
+            Wait();
+            Wait();
+            Wait();
+            Console.WriteLine("Grinding Beans");
+            Wait();
+            Console.WriteLine("Pouring water");
+            Wait();
+            Wait();
+            Console.WriteLine("Coffee is served");
+        }
+
+        public static void Wait(){
+            int time = 3;
+
+            for (int i = 0; i < time; i++)
+            {
+                Console.Write('.');
+                Thread.Sleep(500);
+            }
+
+            Console.WriteLine('.');
+        }
+    }
+}
